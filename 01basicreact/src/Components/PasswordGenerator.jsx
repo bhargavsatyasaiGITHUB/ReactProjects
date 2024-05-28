e31c111ba1346426a819e702b96d784374e18221
Bhargav Satya Sai <bhargavs@vconstruct.in>
2024-05-28 23:05:35 +0530

    did some modifications

--- 01basicreact/src/Components/PasswordGenerator.jsx
+++ 01basicreact/src/Components/PasswordGenerator.jsx
@@ -39,9 +39,9 @@ function PasswordGenerator() {
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-      <h1 className='text-white text-center my-3'>Password generator</h1>
-    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+    <div >
+      <h1 >Password generator</h1>
+    <div >
         <input
             type="text"
             value={password}
@@ -52,12 +52,11 @@ function PasswordGenerator() {
         />
         <button
         onClick={copyPasswordToClipboard}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
         >copy</button>
         
     </div>
-    <div className='flex text-sm gap-x-2'>
-      <div className='flex items-center gap-x-1'>
+    <div >
+      <div >
         <input 
         type="range"
         min={6}
@@ -68,7 +67,7 @@ function PasswordGenerator() {
           />
           <label>Length: {length}</label>
       </div>
-      <div className="flex items-center gap-x-1">
+      <div>
       <input
           type="checkbox"
           defaultChecked={numberAllowed}
@@ -79,7 +78,7 @@ function PasswordGenerator() {
       />
       <label htmlFor="numberInput">Numbers</label>
       </div>
-      <div className="flex items-center gap-x-1">
+      <div >
           <input
               type="checkbox"
               defaultChecked={charAllowed}
